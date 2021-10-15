fea520e45d9b3e8a92406d43f4945823e2049ce0
balajee <baluworks@gmail.com>
2021-10-16 01:28:30 +0530

    added semantic react UI/css and added App component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
+
+import { Button, Container, Form, Grid, Header, Icon, Segment, Statistic } from 'semantic-ui-react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Header as='h1'>Budget</Header>
+
+      <Statistic size='small'>
+        <Statistic.Label>Your Balance:</Statistic.Label>
+        <Statistic.Value>2,550.00</Statistic.Value>
+      </Statistic>
+
+      <Segment textAlign='center'>
+        <Grid columns={2} divided>
+          <Grid.Row>
+            <Grid.Column>
+              <Statistic color='green' size='tiny'>
+                <Statistic.Label style={{ textAlign: 'left' }}>Incoming :</Statistic.Label>
+                <Statistic.Value>1,500.00</Statistic.Value>
+              </Statistic>
+            </Grid.Column>
+            <Grid.Column>
+              <Statistic color='red' size='tiny'>
+                <Statistic.Label style={{ textAlign: 'left' }}>Expenses :</Statistic.Label>
+                <Statistic.Value>1,500.00</Statistic.Value>
+              </Statistic>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+
+      <Header as='h3'>History</Header>
+      <Segment color='red'>
+        <Grid columns={3} textAlign='right'>
+          <Grid.Row>
+            <Grid.Column width={10} textAlign='left' color='yellow'>Something.</Grid.Column>
+            <Grid.Column width={3} textAlign='right' color='blue'>123.00</Grid.Column>
+            <Grid.Column width={3} textAlign='right' color='grey'>
+              <Icon name='edit'></Icon>
+              <Icon name='trash'></Icon>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+      <Header as='h3'>Add New Transaction</Header>
+      <Form>
+        <Form.Group>
+          <Form.Input placeholder='New Tranaction' label='Description' width={12} icon='tags'></Form.Input>
+          <Form.Input placeholder='100.00' label='Value' icon='dollar' iconPosition='left' width={4} ></Form.Input>
+        </Form.Group>
+      </Form>
+      <Button.Group style={{ marginTop: 20 }}>
+        <Button>Cancel</Button>
+        <Button.Or />
+        <Button color='blue'>Ok</Button>
+      </Button.Group>
+    </Container>
   );
 }
 
